b9ef3f7cc7e0c0399c890ea173399f94694f4fe2
ckossendey <81306343+ckossendey@users.noreply.github.com>
2021-04-14 12:58:37 +0200

    Update huumstatus.js

--- huumstatus.js
+++ huumstatus.js
@@ -3,19 +3,19 @@
 const axios = require('axios');
  
 const url = 'https://api.huum.eu/action/home/status';
- 
- 
+
  
 function getHuum () { 
 	axios.get(url, 
 		{ auth: {
-            username: 'user@mail.de',
+            username: 'user',
             password: 'password' 
+			 // enter Username and Password from your App
 			}
 		})
 		.then (function(response) {
 			   console.log(response);
-				// console.log(response.data.statusCode);
+			   // console.log(response.data.statusCode);
 			   // console.log(response.data.config);
 			   // console.log(response.data.light);
 			   // console.log(response.data.paymentEndDate);
@@ -31,6 +31,3 @@ function getHuum () {
 };
 
      getHuum();
-
- 
- 
