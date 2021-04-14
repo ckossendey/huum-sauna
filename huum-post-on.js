6c9d8a66fcbebf8be772cda0bf7120234ad59a24
ckossendey <81306343+ckossendey@users.noreply.github.com>
2021-04-14 13:00:41 +0200

    Create huum-post-on.js

---
+++ huum-post-on.js
@@ -0,0 +1,19 @@
+"use strict"
+ 
+let temperature
+temperature = 50;
+ 
+let data = 'targetTemperature='+ temperature;
+ 
+const axios = require('axios');
+const urlstart = "https://api.huum.eu/action/home/start";
+ 
+axios.post(urlstart,data, 
+    {auth:        {username: 'user',  password: 'pass' }   
+    })
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
