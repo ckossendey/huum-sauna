3bd22e5735c1630ae4d3936921fb511c91363cde
ckossendey <81306343+ckossendey@users.noreply.github.com>
2021-04-14 13:01:28 +0200

    Update huum-post-off.js

--- huum-post-off.js
+++ huum-post-off.js
@@ -11,10 +11,12 @@ const urlstart = "https://api.huum.eu/action/home/stop";
 axios.post(urlstart, data,
     {auth:        {username: 'user',  password: 'pass' }   
     })
-    
+    			 // enter Username and Password from your App
+
   .then(function (response) {
     console.log(response.data);
   })
+
   .catch(function (error) {
     console.log(error);
   });
