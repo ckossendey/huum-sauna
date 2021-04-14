49db1d16a0ce20acecd9de5ca168cf030e3e7727
ckossendey <81306343+ckossendey@users.noreply.github.com>
2021-04-14 12:59:43 +0200

    Create huum-post-off.js

---
+++ huum-post-off.js
@@ -0,0 +1,25 @@
+"use strict"
+ 
+let temperature
+temperature = 50;
+ 
+let data = "off";
+ 
+const axios = require('axios');
+const urlstart = "https://api.huum.eu/action/home/stop";
+ 
+axios.post(urlstart, data,
+    {auth:        {username: 'user',  password: 'pass' }   
+    })
+    
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+ 
+ 
+ 
+ 
+ 
